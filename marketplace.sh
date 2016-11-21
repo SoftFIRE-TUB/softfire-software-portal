7f1200d1c897b37bc2e19a98d64f58948a424dfe
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-11-21 12:37:11 +0100

    Fix: marketplace script

--- marketplace.sh
+++ marketplace.sh
@@ -3,4 +3,4 @@
 if [ ! -z "$1" ] && [ $1 == "compile" ]; then
 	./gradlew build -x test
 fi
-screen -d -m -S marketplace java -jar build/libs/marketplace-1.0.jar --spring.config.location=file:/etc/openbaton/marketplace/marketplace.properties
+screen -d -m -S marketplace java -jar build/libs/softfire-software-portal-1.0.jar --spring.config.location=file:/etc/openbaton/marketplace/marketplace.properties
